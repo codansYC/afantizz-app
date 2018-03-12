52b9e8bca1f7f842aa85b16f9128a78fc4374ce8
codansYC <yuanchao@chushi007.com>
2018-03-12 16:56:13 +0800

    -

--- config/db.php
+++ config/db.php
@@ -20,7 +20,7 @@ $debugDB = [
 
 $DB = [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=afantizz',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=afantizz',
     'username' => 'root',
     'password' => '123456',
     'charset' => 'utf8',
